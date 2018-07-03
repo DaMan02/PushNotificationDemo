b79ff6b28e49589477ef8bcaf3b075603e235fc3
Da Man <mdayal02@gmail.com>
2018-07-03 16:37:13 +0530

    Initial commit

---
+++ app/src/main/java/com/dayal/pushnotification/Config.java
@@ -0,0 +1,20 @@
+package com.dayal.pushnotification;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Config {
+
+    // global topic to receive app wide push notifications
+    public static final String TOPIC_GLOBAL = "global";
+
+    // broadcast receiver intent filters
+    public static final String REGISTRATION_COMPLETE = "registrationComplete";
+    public static final String PUSH_NOTIFICATION = "pushNotification";
+
+    // id to handle the notification in the notification tray
+    public static final int NOTIFICATION_ID = 100;
+    public static final int NOTIFICATION_ID_BIG_IMAGE = 101;
+
+    public static final String SHARED_PREF = "ah_firebase";
+}
